b579a077f07e2502a3d88c754d4146affce14e6f
wspenc <105823133+wspenc@users.noreply.github.com>
2022-09-14 08:14:08 -0600

    Add files via upload
    Find the range challenge

---
+++ findTheRange.js
@@ -0,0 +1,8 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+for(let i = 0; i < 1; i++){
+    console.log(array[6] + " is the smallest number");
+    console.log(array[1] + " is the largest number");
+}
